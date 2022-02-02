cdf6e03af5715eed00f1be300a91d765be3063bf
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-02-02 18:36:24 +0000

    Fix tests errors

--- common/tests.py
+++ common/tests.py
@@ -11,20 +11,20 @@ class TestPages(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual('/', reverse('home'))
-        self.assertTemplateUsed(response, template_name='organization/home.html')
+        self.assertTemplateUsed(response, template_name='common/home.html')
         # self.assertContains(response, "", html=True)
 
     def test_about_page(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual('/about/', reverse('about'))
-        self.assertTemplateUsed(response, template_name='organization/about.html')
+        self.assertTemplateUsed(response, template_name='common/about.html')
         # self.assertContains(response, "", html=True)
 
     def test_roadmap_page(self):
         response = self.client.get('/roadmap/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual('/roadmap/', reverse('roadmap'))
-        self.assertTemplateUsed(response, template_name='organization/roadmap.html')
+        self.assertTemplateUsed(response, template_name='common/roadmap.html')
         # self.assertContains(response, "", html=True)
 
