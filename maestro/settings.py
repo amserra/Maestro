5a537053da1edd426b2eb45872fbbea91cb27442
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-02-03 13:27:54 +0000

    Update settings.py

--- maestro/settings.py
+++ maestro/settings.py
@@ -128,10 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static_files/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static_files',
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
