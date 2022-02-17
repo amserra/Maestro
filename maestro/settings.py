346cc8b9bae49d8feeafa16b530e3af4b76c4ed7
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-02-17 15:15:46 +0000

    Update settings

--- maestro/settings.py
+++ maestro/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_unsecure_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
