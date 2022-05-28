716178be63895324fb28aba0d47ec8e8a27ebe7a
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-05-28 19:59:46 +0100

    Small update on deploy

--- deploy/ansible-playbooks/run-all.sh
+++ deploy/ansible-playbooks/run-all.sh
@@ -8,3 +8,8 @@ ansible-playbook install-ssl.yml
 # 1 - Create superuser (python manage.py createsuperuser, input credentials)
 # 2 - Answer certbot prompts when installing ssl
 # 3 - Copy classifiers/flood_depth_weights.hdf5 to machine (file to big for source control)
+# Note: If /media files aren't showing, the problem is probably with permissions.
+# To confirm this, check the nginx error logs: tail -10 /var/log/nginx/error.log
+# If says something like "Permission denied", it confirms.
+# The user may have permissions for the folder /media, but may not have for the folders above
+# To solve, check the internet. In extreme case, give permissions to all (chmod 777) to the folders above /media
