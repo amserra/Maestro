873249990db80f789f5eac8aeb35eeaf54b63dd5
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2023-02-01 10:54:03 +0000

    Updates on deployment files

--- deploy/ansible-playbooks/run-all.sh
+++ deploy/ansible-playbooks/run-all.sh
@@ -7,7 +7,7 @@ ansible-playbook install-ssl.yml
 # Additional steps that arent very well automated:
 # 1 - Create superuser (python manage.py createsuperuser, input credentials)
 # 2 - Answer certbot prompts when installing ssl
-# 3 - Copy classifiers/flood_depth_weights.hdf5 to machine (file to big for source control)
+# 3 - Copy classifiers/flood_depth/weights.hdf5 to machine (file to big for source control)
 # 4 - Make docs: cd docs && make html
 # 5 - Create components by 'python manage.py shell' and importing main from context/create_existing_plugins.py
 # Note: If /media files aren't showing, the problem is probably with permissions.
@@ -15,3 +15,5 @@ ansible-playbook install-ssl.yml
 # If says something like "Permission denied", it confirms.
 # The user may have permissions for the folder /media, but may not have for the folders above
 # To solve, check the internet. In extreme case, give permissions to all (chmod 777) to the folders above /media
+# Problems related with Oracle and port opening:
+# https://stackoverflow.com/questions/62326988/cant-access-oracle-cloud-always-free-compute-http-port
--- deploy/gunicorn/prod.py
+++ deploy/gunicorn/prod.py
@@ -3,11 +3,11 @@
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "maestro.wsgi:application"
 # The granularity of Error log outputs
-loglevel = "debug"
+loglevel = "error"
 # The number of worker processes for handling requests
 workers = 2
 # The socket to bind
-bind = "unix:/run/gunicorn/gunicorn.sock"
+bind = "unix:/run/gunicorn.sock"
 # Write access and error info to /var/log
 accesslog = errorlog = "/var/log/gunicorn/prod.log"
 # Redirect stdout/stderr to log file
